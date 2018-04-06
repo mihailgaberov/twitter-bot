6237401fea04850c88e89e0675ee4d698390ec10
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-04-06 06:38:37 +0200

    Added the main logic.

--- bot.js
+++ bot.js
@@ -1,3 +1,42 @@
 import twit from 'twit'
 import config from './config'
 
+const Twitter = new twit(config)
+
+const sendDirectMessage = function () {
+  const params = {
+    q: '#js, #javascript, #JavaScript, #JS, #Javascript, #react, #reactjs, #nodejs, #Nodejs',
+    result_type: 'recent',
+    lang: 'en'
+  }
+  Twitter.get('search/tweets', params, function (err, data) {
+    if (!err) {
+      const tweetId = data.statuses[0].id_str
+
+      Twitter.post('direct_messages/events/new', {
+        'type': 'message_create',
+        'message_create': {
+          'target': {
+            'recipient_id': '390602997'
+          },
+          'message_data': {
+            'text': 'Tweet with ID: ' + tweetId,
+          }
+        }
+      }, function (err, response) {
+        if (response) {
+          console.log('Message sent. Response: ', response)
+        }
+        if (err) {
+          console.log('Something went wrong while sending message. Error: ', err)
+        }
+      })
+    } else {
+      console.log('Something went wrong while searching.')
+    }
+  })
+}
+
+sendDirectMessage()
+// every 60 minutes
+setInterval(sendDirectMessage, 3600000)
