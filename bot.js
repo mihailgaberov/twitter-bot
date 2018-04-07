f639ebf36de3a3b9bfaeb8c7fdb0b6d7ba9e7caa
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-04-07 12:32:56 +0200

    Update the content of the message to include all available tweets according to the search criteria being recent.

--- bot.js
+++ bot.js
@@ -11,15 +11,20 @@ const sendDirectMessage = function () {
   }
   Twitter.get('search/tweets', params, function (err, data) {
     if (!err) {
-      const tweetId = data.statuses[0].id_str
-      const createdAt = data.statuses[0].created_at
-      const source = data.statuses[0].source
-      const tweetText = data.statuses[0].text
-      const userThatTweeted = data.statuses[0].user.name
-      const userDescription = data.statuses[0].user.description
-      const userFollowersCount = data.statuses[0].user.followers_count
-      const userFriendsCount = data.statuses[0].user.friends_count
-      const userStatusesCount = data.statuses[0].user.statuses_count
+      let content = `=========== Daily report (${data.statuses.length})=================`
+
+      data.statuses.forEach((status) => {
+        content += `--------------- Tweet ID: ${status.id_str} ---------------              
+              Created at: ${status.created_at}
+              Source: ${status.source}
+              Status text: ${status.text}
+              By: ${status.user.name}
+              With desc: ${status.user.description}
+              With: ${status.user.followers_count} followers
+              And: ${status.user.friends_count} friends
+              Tweeted so far: ${status.user.statuses_count} statuses.\`,
+`
+      })
 
       Twitter.post('direct_messages/events/new', {
         event: {
@@ -29,15 +34,7 @@ const sendDirectMessage = function () {
               'recipient_id': '390602997'
             },
             'message_data': {
-              'text': `Tweet ID: ${tweetId}
-              Created at: ${createdAt}
-              Source: ${source}
-              Content: ${tweetText}
-              By: ${userThatTweeted}
-              With desc: ${userDescription}
-              With ${userFollowersCount} followers
-              And ${userFriendsCount} friends
-              Tweeted so far ${userStatusesCount} statuses.`,
+              'text': content,
             }
           }
         }
