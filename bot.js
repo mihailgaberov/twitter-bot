a1a7dc93780b8562021cbb2eb2722af1a9353d81
Mihail Gaberov <mihail.gaberov@gmail.com>
2020-03-30 12:51:39 +0300

    :racehorse: Removed functionality for sending direct messages.

--- bot.js
+++ bot.js
@@ -17,43 +17,7 @@ const sendDirectMessage = function () {
   Twitter.get('search/tweets', params, function (err, data) {
     if (!err) {
       db.getUniqueTweets(data.statuses).then((tweets) => {
-        const content = helpers.composeContent(tweets)
         db.recordUniqueTweets(tweets)
-
-        Twitter.get('followers/ids', {
-          user_id: 982290988562608129,
-          screen_name: 'herrklinkerhof3'
-        }, (err, response) => {
-          if (err) {
-            console.log('Something went wrong while getting followers. Error: ', err)
-          }
-  
-          if (response) {
-            response.ids.forEach((id) => {
-              Twitter.post('direct_messages/events/new', {
-                event: {
-                  'type': 'message_create',
-                  'message_create': {
-                    'target': {
-                      'recipient_id': id
-                    },
-                    'message_data': {
-                      'text': content,
-                    }
-                  }
-                }
-              }, (err, response) => {
-                if (err) {
-                  console.log('Something went wrong while sending message. Error: ', err)
-                } else {
-                  if (response) {
-                    console.log('Message sent. Response: ', response)
-                  }
-                }
-              })
-            })
-          }
-        })
       })
     } else {
       console.log('Something went wrong while searching.')
